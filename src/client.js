6ee4756021b872bc8ad2bd4cc2b46468b9433df8
BrianZhang42 <brianzhang42@gmail.com>
2022-04-15 16:58:30 -0400

    changes in user files

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import * as article from './article-viewmodel.js';
 import * as user from './user-viewmodel.js';
+import * as userUtil from './server/userUtil.js';
+import { checkRegister } from '../server/userUtil.js';
 
 const searchForm = document.getElementById("searchForm");
 searchForm.addEventListener("submit", event => {
@@ -12,28 +14,12 @@ let password2 = document.getElementById("pass2");
 let email = document.getElementById("email");
 let username = document.getElementById("username");
 document.getElementById("register").addEventListener("click", async () => {
-    if (password.value != password2.value) {
-        alert("Passwords do not match");
-        return;
-    }
-    else if (password.value === null || password2.value === null) {
-        alert("No password entered");
-        return;
-    }
-    else if (email.value === null) {
-        alert("No email entered");
-        return;
-    }
-    else if (username.value === null) {
-        alert("No password entered");
-        return;
-    }
-    
-    let formData = {
-        username: username.value,
-        email: email.value,
-        password: password.value
-    }
+    if (checkRegister())
+    // let formData = {
+    //     username: username.value,
+    //     email: email.value,
+    //     password: password.value
+    // }
 
     user.createUser(formData);    
 });
