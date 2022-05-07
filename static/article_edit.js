bb51384df96d7292648e427042480bb708d03317
Neil Gupta <nogupta@umass.edu>
2022-05-07 13:12:05 -0400

    edit preview

--- static/article_edit.js
+++ static/article_edit.js
@@ -1,93 +1,21 @@
 import * as article from "./article-viewmodel.js";
 
 const title = document.getElementById("article-title");
-const content = document.getElementById("article-content");
+const contentPreview = document.getElementById("article-preview");
+const contentTextbox = document.getElementById("editBox");
 
-let currentArticle = undefined;
+const articleID = window.location.pathname.slice(9, -5);
+const currentArticle = await article.readArticle(articleID);
 
-if(document.readyState !== "loading") {
-    loadContent();
-}
-else {
-    window.addEventListener("load", loadContent);
-}
+// set title
+title.innerText = currentArticle.title;
+const DOMTitle = document.createElement("title");
+DOMTitle.innerText = `Bitwikk - Editing ${currentArticle.title}`;
+document.head.appendChild(DOMTitle);
 
-async function loadContent() {
-    const articleID = window.location.pathname.slice(9, -5);
-    currentArticle = await article.readArticle(articleID);
-
-    // set title
-    title.innerText = currentArticle.title;
-
-    // // set body content
-    // currentArticle.content.forEach((topicDetails, index) => {
-    //     let topicTitle = document.createElement("h2");
-    //     topicTitle.setAttribute("id", `topic-title-${index}`);
-    //     content.appendChild(topicTitle);
-    //     if(topicDetails.title != undefined)
-    //         topicTitle.innerText = topicDetails.title;
-
-    //     let topicBody = document.createElement("p");
-    //     topicBody.setAttribute("id", `topic-body-${index}`);
-    //     content.appendChild(topicBody);
-    //     if(topicDetails.body != undefined)
-    //         topicBody.innerText = topicDetails.body;
-
-    // });
-
-    // // set contributors
-    // let contributorString = "Contributors: ";
-    // currentArticle.contributors.forEach(contributor => contributorString += `${contributor}, `)
-    // contributors.innerText = contributorString;
-
-    // // set details
-    // currentArticle.details.forEach((detail, index) => {
-    //     let listElem = document.createElement("li");
-    //     let aElem = document.createElement("a");
-    //     aElem.innerText = detail;
-    //     aElem.setAttribute("href", "http://www.google.com");
-
-    //     listElem.appendChild(aElem);
-    //     listElem.setAttribute("id", `detail-${index}`);
-    //     details.appendChild(listElem);
-    // })
-
-    // // set related topics
-    // currentArticle.relatedTopics.forEach((topic, index) => {
-    //     let listElem = document.createElement("li");
-    //     let aElem = document.createElement("a");
-    //     aElem.innerText = topic;
-    //     aElem.setAttribute("href", "http://www.google.com");
-
-    //     listElem.appendChild(aElem);
-    //     listElem.setAttribute("id", `related-topic-${index}`);
-    //     relatedTopics.appendChild(listElem);
-    // })
-
-    // // set images
-    // primaryImage.setAttribute("src", currentArticle.images[0]);
-    // currentArticle.images.forEach((image, index) => {
-    //     let carousel = index < 3 ? activeCarouselImages : inactiveCarouselImages;
-    //     let colDiv = document.createElement("div");
-    //     let img = document.createElement("img");
-
-    //     colDiv.setAttribute("class", "col")
-    //     colDiv.appendChild(img);
-
-    //     img.setAttribute("class", "img-fluid");
-    //     img.setAttribute("src", image);
-
-    //     carousel.appendChild(colDiv);
-    // })
-
-    // set title
-    // set title
-    const DOMTitle = document.createElement("title");
-    DOMTitle.innerText = `Bitwikk - Editing ${articleID}`;
-    document.head.appendChild(DOMTitle);
-
-    // document.getElementById("editPage").href = `/article/${articleID}/edit`;
-}
+contentTextbox.addEventListener("input", event => {
+    contentPreview.innerHTML = marked.parse(contentTextbox.value);
+});
 
 // document.getElementById('post').addEventListener("click", async event => {
 //     // const comment = document.createElement('div');
