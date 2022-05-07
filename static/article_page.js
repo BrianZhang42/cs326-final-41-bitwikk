a38c629fefbd6af8f3a150914e04bf0bc482ba62
Neil Gupta <nogupta@umass.edu>
2022-05-07 12:28:53 -0400

    work on article editing

--- static/article_page.js
+++ static/article_page.js
@@ -22,7 +22,7 @@ else {
 }
 
 async function loadContent() {
-    let articleID = window.location.pathname.substring(9);
+    const articleID = window.location.pathname.substring(9);
     currentArticle = await article.readArticle(articleID);
 
     // set title
@@ -90,62 +90,11 @@ async function loadContent() {
         carousel.appendChild(colDiv);
     })
 
+    // set title
+    const DOMTitle = document.createElement("title");
+    DOMTitle.innerText = `Bitwikk - ${articleID}`;
+    document.head.appendChild(DOMTitle);
+
+    // set edit button href
     document.getElementById("editPage").href = `/article/${articleID}/edit`;
 }
-
-// document.getElementById('editPage').addEventListener("click", editContent);
-
-// function editContent() {
-//     if (!document.getElementById('mainArt').isContentEditable) {
-//         document.getElementById('mainArt').contentEditable = true;
-//     } else {
-//         document.getElementById('mainArt').contentEditable = false;
-//     }
-// }
-
-// document.getElementById('post').addEventListener("click", async event => {
-//     // const comment = document.createElement('div');
-//     // comment.appendCHild(document.getElementById('comment').value);
-//     // document.getElementById('comment').value = '';
-//     // document.getElementById('csection').appendChild(comment);
-//     if(currentArticle == undefined) {
-//         console.error(`no article was loaded while submitting article edit.`);
-//     }
-
-//     let children = content.childNodes;
-//     let topicTitles = [];
-//     let topicBodies = [];
-//     children.forEach((child, index) => {
-//         if(child.id === undefined || child.innerText == undefined)
-//             return;
-
-//         if(child.id.substring(0, 12) === "topic-title-") {
-//             let topicNum = child.id.substring(12, 13);
-//             topicTitles[topicNum] = child.innerText;
-//         }
-//         else if(child.id.substring(0, 11) === "topic-body-") {
-//             let topicNum = child.id.substring(11, 12);
-//             topicBodies[topicNum] = child.innerText;
-//         }
-//     });
-
-//     if(topicBodies.length != topicTitles.length) {
-//         console.error("While generating article update request body, didn't find an equal "
-//             + "number of article topic titles and article topic bodies.");
-//         return;
-//     }
-
-//     let newContent = [];
-//     topicBodies.forEach((body, index) => {
-//         let title = topicTitles[index];
-//         newContent.push({title: title, body: body});
-//     });
-
-//     let formData = {
-//         ID: currentArticle.ID,
-//         title: title.innerText,
-//         content: newContent,
-//     }
-
-//     article.updateArticle(formData);
-// });
