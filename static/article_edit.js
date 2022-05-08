5907b1b0449ccc1bdf81709c4496115a3e1d59e3
Neil Gupta <nogupta@umass.edu>
2022-05-07 22:12:54 -0400

    wip gallery editing

--- static/article_edit.js
+++ static/article_edit.js
@@ -3,6 +3,7 @@ import * as article from "./article-viewmodel.js";
 const title = document.getElementById("article-title");
 const contentPreview = document.getElementById("article-preview");
 const contentTextbox = document.getElementById("editBox");
+const galleryTextbox = document.getElementById("galleryEditBox");
 const saveButton = document.getElementById("saveChanges");
 
 const articleID = window.location.pathname.slice(9, -5);
@@ -20,6 +21,7 @@ contentTextbox.addEventListener("input", event => {
 
 contentTextbox.value = currentArticle.content;
 contentPreview.innerHTML = marked.parse(contentTextbox.value);
+galleryTextbox.value = currentArticle.images.join("\n");
 
 saveButton.addEventListener("click", async event => {
     const response = await article.updateArticle(articleID, {content: contentTextbox.value});
